8c27d76d69cb1a506ceda2636cbb8ab10dae44b2
LianAustin <austin.lian25@ycdsbk12.ca>
2024-02-14 14:24:01 -0500

    modified:   Rads.java

--- Rads.java
+++ Rads.java
@@ -1,13 +1,24 @@
 class Rads extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A program that converts degrees to radians given an input in radians
+  * @author: Austin Lian
   */
   
   public void run() {
     
-    // start coding here
+    // declare variables
+    double dblRadians;
+    double dblDegrees;
+
+    // get degrees
+    dblDegrees = readDouble("Enter the degrees: ");
+
+    // calculate radians
+    dblRadians = Math.toRadians(dblDegrees);
+
+    // output results
+    System.out.println(dblDegrees + " degrees is " + dblRadians + " radians");
     
   }
 }
