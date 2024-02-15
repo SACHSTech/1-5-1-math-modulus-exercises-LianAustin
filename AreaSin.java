1bee3ed106ac2578d284417d673309fb42075e16
LianAustin <austin.lian25@ycdsbk12.ca>
2024-02-15 09:49:33 -0500

    modified:   AreaSin.java

--- AreaSin.java
+++ AreaSin.java
@@ -1,13 +1,27 @@
 class AreaSin extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A program that that uses the Math.sin() function to calculate the area of a triangle, given two sides and the contained angle
+  * @author: Austin Lian
   */
   
   public void run() {
-    
-    // start coding here
+    // Initialize variables
+    double dblSideA;
+    double dblSideB;
+    double dblAngle;
+    double dblArea;
+
+    // Get inputs (Sides and Angle)
+    dblSideA = readDouble("Enter the length of side A: ");
+    dblSideB = readDouble("Enter the length of side B: ");
+    dblAngle = readDouble("Enter the angle between the two sides: ");
+
+    // Calculate Area
+    dblArea = (dblSideA * dblSideB * Math.sin(Math.toRadians(dblAngle))) / 2;
+
+    // Output results
+    System.out.println("The area of the triangle is " + dblArea);
     
   }
 }
