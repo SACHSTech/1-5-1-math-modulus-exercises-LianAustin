117da8fcfd0de703eb9b6c1d28b0f1d1c872e534
LianAustin <austin.lian25@ycdsbk12.ca>
2024-02-15 10:10:41 -0500

    Solved Power Problem

--- Power.java
+++ Power.java
@@ -1,13 +1,26 @@
 class Power extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A program which takes an input of two numbers a and b and outputs a^b
+  * @author: Austin Lian
   */
   
   public void run() {
     
-    // start coding here
+    // Initialize variables;
+    int intA;
+    int intB;
+    int intPower;
+
+    // Get inputs (a and b)
+    intA = readInt("Enter the base of the power: ");
+    intB = readInt("Enter the exponent of the power: ");
+
+    // Calculate a^b
+    intPower = (int) Math.pow(intA, intB);
+
+    // Output results
+    System.out.println(intA + " to the power of " + intB + " is " + intPower);
     
   }
 }
